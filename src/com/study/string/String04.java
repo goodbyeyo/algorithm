618bc4e1c2bee3eb4df337126f96f579b9440471
yoonseol <yoonseol1983@gmail.com>
2022-02-23 23:59:28 +0900

    reverse with StringBuilder and while

--- src/com/study/string/String04.java
+++ src/com/study/string/String04.java
@@ -2,6 +2,12 @@ package com.study.string;
 
 import java.util.*;
 
+/**
+ * [설명] N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램 작성
+ * [입력] 첫 줄에 자연수 N(3 <= N <= 20) 이 주어진다
+ * [입력] 두번째 줄부터 N개의 단어가 각 줄어 하나씩 주어진다. 단어는 영어 알파멧으로만 구성
+ * [출력] N개의 단어를 입력된 순서대로 한줄에 하나씩 뒤집어서 출력한다
+ */
 public class String04 {
     public static void main(String[] args) {
         String04 T = new String04();
@@ -19,8 +25,25 @@ public class String04 {
 
     private ArrayList<String> solution(int n, String[] str) {
         ArrayList<String> answer = new ArrayList<>();
+        // 1) 전체 뒤집기
+//        for (String x : str) {
+//            String tmp = new StringBuilder(x).reverse().toString();
+//            answer.add(tmp);
+//        }
+
+        // 2) 쌍으로 뒤집기
         for (String x : str) {
-            String tmp = new StringBuilder(x).reverse().toString();
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
         }
         return answer;
     }
