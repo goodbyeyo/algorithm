940b9e9b925099840c7c9e1904c1956aebb7ecaf
yoonseol <yoonseol1983@gmail.com>
2022-04-17 23:59:49 +0900

    HashMap 모든 아나그램찾기

---
+++ src/com/study/hash/HashMap04.java
@@ -0,0 +1,39 @@
+package com.study.hash;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashMap04 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        System.out.print(solution(a, b));
+    }
+
+    private static int solution(String a, String b) {
+        int answer = 0;
+        HashMap<Character, Integer> am = new HashMap<>();
+        HashMap<Character, Integer> bm = new HashMap<>();
+        for (char x : b.toCharArray()) {
+            bm.put(x, bm.getOrDefault(x, 0) + 1);
+        }
+        int L = b.length() - 1;
+        for (int i = 0; i < L; i++) {
+            am.put(a.charAt(i), am.getOrDefault(a.charAt(i), 0) + 1);
+        }
+        int lt = 0;
+        for (int rt = L; rt < a.length(); rt++) {
+            am.put(a.charAt(rt), am.getOrDefault(a.charAt(rt), 0) + 1);
+            if (am.equals(bm)) {
+                answer++;
+            }
+            am.put(a.charAt(lt), am.get(a.charAt(lt)) - 1);
+            if (am.get(a.charAt(lt)) == 0) {
+                am.remove(a.charAt(lt));
+            }
+        }
+        return answer;
+    }
+
+}
