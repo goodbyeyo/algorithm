27c179ffe64c337d1c148edcb3ff395f19d57b4d
yoonseol <yoonseol1983@gmail.com>
2022-04-24 23:59:32 +0900

    K번째 큰수

--- src/com/study/hash/HashMap04.java
+++ src/com/study/hash/HashMap04.java
@@ -1,4 +1,16 @@
 package com.study.hash;
+/**
+ * 모든 아나그램 찾기(해쉬, 투포인터, 슬라이딩 윈도우)
+ * S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하세요
+ * 아나그램 판별시 대소문자가 구분됩니다, 부분문자열은 연속된 문자열이어야 합니다
+ * [입력설명] 첫 줄에 첫 번째 S 문자열이 입력되고, 두 번째 줄에 T 문자열이 입력됩니다.
+ * S문자열의 길이는 10,000을 넘지 않으며, T문자열은 S문자열보다 길이가 작거나 같습니다.
+ * [출력설명] S단어에 T문자열과 아나그램이 되는 부분문자열의 개수를 출력합니다.
+ * [입력예제] bacaAacba
+ *           abc
+ * [출력예제] 3
+ * 시간복잡도 O(n)
+ */
 
 import java.util.HashMap;
 import java.util.Scanner;
@@ -32,6 +44,7 @@ public class HashMap04 {
             if (am.get(a.charAt(lt)) == 0) {
                 am.remove(a.charAt(lt));
             }
+            lt++;
         }
         return answer;
     }
---
+++ src/com/study/hash/HashMap05.java
@@ -0,0 +1,61 @@
+package com.study.hash;
+
+/**
+ * K번째 큰 수
+ * 1부터 100사이 자연수가 전힌 N장의 카드가 있다.
+ * 같은 숫자의 카드가 여러장 있을 수 있다
+ * 이 중 3장의 카드를 뽑아 각 카드에 적힌 수를 합한 값을 기록 하려고 한다
+ * 3장을 뽑을 수 있는 모든 경우를 기록한다
+ * 기록 한 값 중 K번째로 큰 수를 출력하는 프로그램을 작성한다
+ * 만약 큰 수로부터 만들어진 수가 25 25 23 23 2 20 19 이고 K 값이 3이라면 K번째 큰값은 22
+ * [입력설명] 첫 줄에 자연수 N(3<=N<=100)과 K(1<=K<=50) 입력되고, 그 다음 줄에 N개의 카드값이 입력된다
+ * [출력설명] 첫 줄에 K번째 수를 출력한다. K번째 수가 존재하지 않으면 -1를 출력한다.
+ * [입력예제]
+ * 10 3
+ * 13 15 34 23 45 65 33 11 26 42
+ * [출력예제]
+ * 143
+ */
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class HashMap05 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        System.out.println(solution(arr, n, k));
+    }
+
+    private static int solution(int[] arr, int n, int k) {
+        int answer = 0;
+        TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder());  // 내림차순으로 자동정렬
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int l = j + 1; l < n; l++) {
+                    Tset.add(arr[i] + arr[j] + arr[l]);
+                }
+            }
+        }
+        int cnt = 0;
+        // Tset.remove(143); Tset.remove : 제거
+        // Tset.size(); // 원소의 갯수 출력
+        // Tset.first(); // 오름차순 -> 최소값, 내림차순 : 최대값
+        // Test.last();  // 오름차순 -> 최대값, 내림차순 : 최소값
+        for (int x : Tset) {
+            cnt++;
+            if (cnt == k) {
+                return x;
+            }
+//            System.out.println(cnt+"-"+x);
+        }
+        return answer;
+    }
+
+}
