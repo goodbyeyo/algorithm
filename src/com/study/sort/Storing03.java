5711a4122d3778d657c60f5a10a22e236199b74c
yoonseol <yoonseol1983@gmail.com>
2022-06-14 23:58:06 +0900

    삽입정렬

--- src/com/study/sort/Storing03.java
+++ src/com/study/sort/Storing03.java
@@ -3,14 +3,13 @@ package com.study.sort;
 import java.util.Scanner;
 
 /**
- * 버블 정렬
+ * 삽입 정렬
  * [설명] N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성
  * [출력] 오름차순으로 정렬된 수열을 출력
- * [예시 입력] 6 //  13 5 11 7 23 15
- * [예시 출력] 5 7 11 13 15 23
+ * [예시 입력] 6 // 11 7 5 6 10 9
+ * [예시 출력] 5 6 7 9 10 11
  */
-
-public class Sorting02 {
+public class Storing03 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
@@ -21,17 +20,21 @@ public class Sorting02 {
         for (int x : solution(n, arr)) {
             System.out.print(x + " ");
         }
+
     }
 
     private static int[] solution(int n, int[] arr) {
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int j = 0;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > tmp) {
+                    arr[j + 1] = arr[j];
+                } else {
+                    break;
                 }
             }
+            arr[j + 1] = tmp;
         }
         return arr;
     }
