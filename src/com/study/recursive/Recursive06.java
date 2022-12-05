16ad48d8f83af3c30dc7a7b99a4f740ea6fe9cc0
Sangwook Hwang <yoonseol1983@gmail.com>
2022-12-06 00:05:21 +0900

    DFS 부분집합 오류 수정

--- src/com/study/recursive/Recursive06.java
+++ src/com/study/recursive/Recursive06.java
@@ -1,5 +1,16 @@
 package com.study.recursive;
 
+/**
+ * 첫번째 줄부터 각 줄에 하나씩 부분집합을 아래와 같은 순서로 출력, 단 공집합은 출력 X
+ * [입력 예시] 3
+ * [출력 예시] 1 2 3
+ *           1 2
+ *           1 3
+ *           1
+ *           2 3
+ *           2
+ *           3
+ */
 public class Recursive06 {
 
     static int n;
@@ -15,13 +26,13 @@ public class Recursive06 {
     private void DFS(int L) {
         if (L == n + 1) {
             String tmp = "";
-            for (int i = 0; i < n; i++){
+            for (int i = 0; i <= n; i++){
                 if (ch[i] == 1) {
                     tmp += (i + " ");
                 }
-                if (tmp.length() > 0) {
-                    System.out.println(tmp;
-                }
+            }
+            if (tmp.length() > 0) {
+                System.out.println(tmp);
             }
         }else {
             ch[L] = 1;
