1f731c057cb6ac3ff1939baf64662b280152da63
Hwang Sangwook <goodbyeyo@naver.com>
2023-12-08 18:30:25 +0900

    add method in doubleLinkedList

--- src/com/study/linkedList/DoubleLinkedList.java
+++ src/com/study/linkedList/DoubleLinkedList.java
@@ -41,18 +41,6 @@ public class DoubleLinkedList<T> {
         }
     }
 
-    public static void main(String[] args) {
-        DoubleLinkedList<Integer> myLinkedList = new DoubleLinkedList<>();
-        myLinkedList.addNode(1);
-        myLinkedList.addNode(3);
-        myLinkedList.addNode(5);
-        myLinkedList.addNode(7);
-        myLinkedList.addNode(9);
-        myLinkedList.searchFromTail(7);
-        myLinkedList.searchFromHead(3);
-        myLinkedList.printAll();
-    }
-
     public T searchFromHead(T isData) {
         if (this.head == null) {
             return null;
@@ -81,5 +69,60 @@ public class DoubleLinkedList<T> {
             System.out.println(node.data);
         }
         return null;
+    }
+
+    public boolean insertToFront(T existedData, T addData) {
+        if (this.head == null) {
+            this.head = new Node<T>(addData);
+            this.tail = this.head;
+            return true;
+        } else if (this.head.data == existedData) {
+            Node<T> newHead = new Node<T>(addData);
+            newHead.next = this.head;
+            this.head = newHead;
+            return true;
+        } else {    // 전체 순회
+            Node<T> node = this.head;
+            while (node != null) {
+                if (node.data == existedData) {
+// prev node.prev node.prev.next, newPrev.prev newPrev newPrev.next node.prev node next
+                    Node<T> newPrev = new Node<T>(addData);
+                    node.prev.next = newPrev;
+                    newPrev.prev = node.prev;
+                    newPrev.next = node;
+                    node.prev = newPrev;
+                    return true;
+
+// other solution (is same?)
+// prev nodePrev next , prev, nodePrev.next, nextPrev node next
+//                    Node<T> nodePrev = node.prev;
+//                    nodePrev.next = new Node<T>(addData);
+//                    nodePrev.next.next = node;
+//                    nodePrev.next.prev = nodePrev;
+//                    node.prev = nodePrev.next;
+//                    return true;
+                } else {    // not found existedData
+                    node = node.next;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        DoubleLinkedList<Integer> myLinkedList = new DoubleLinkedList<>();
+        myLinkedList.addNode(1);
+        myLinkedList.addNode(3);
+        myLinkedList.addNode(5);
+        myLinkedList.addNode(7);
+        myLinkedList.addNode(9);
+        myLinkedList.searchFromTail(7);
+        myLinkedList.searchFromHead(3);
+        myLinkedList.insertToFront(3, 2);
+        myLinkedList.insertToFront(5, 4);
+        myLinkedList.insertToFront(7, 6);
+        myLinkedList.insertToFront(9, 8);
+        System.out.println("---------------------");
+        myLinkedList.printAll();
     }
 }
